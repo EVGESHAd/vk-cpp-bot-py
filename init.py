c4eb3f72eb272027e9cb1f4e5dfdd3ad3da2f16f
EVGESHAd <evgeniy.d@informmonitoring.ru>
2018-05-31 20:07:52 +0300

    add xash server list

--- init.py
+++ init.py
@@ -1,3 +1,5 @@
-print("py init")
+# -*- coding: utf-8 -*-
+print('py init')
 #     cmd      path    disp info cost acess
-init("i", "example.py", 1, "info", 0, 1)
+init('i', 'example.py', 1, 'info', 0, 1)
+init('ms', 'ms_xash.py', 1, 'xash servers', 0, 1)
---
+++ ms_xash.py
@@ -0,0 +1,33 @@
+import socket
+import struct
+import time
+import re
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+adr=('ms.xash.su', 27010)
+sock.bind(('0.0.0.0', 0))
+sock.sendto('1\xff0.0.0.0:0\0\0', adr)
+data = sock.recvfrom(4096)
+s = 6;
+servs={}
+while True:
+	IP = struct.unpack('BBBB',data[0][s:s+4])
+	ipstr = '%d.%d.%d.%d' % IP
+	port = struct.unpack('>H',data[0][s+4:s+6])
+	if port[0] == 0: break
+	sock.sendto('\xff\xff\xff\xffinfo 48',(ipstr,port[0]))
+	s = s + 6
+starttime = time.time()
+sock.setblocking(0)
+while time.time() - starttime < 10:
+	try:
+		serv = re.sub(r'\^\d{1}', '', sock.recvfrom(4096)[0][10:]).split('\\')
+		servs[serv[15]]=servs.get(serv[15], [])+[[serv[1]]+[serv[3]]+[serv[11]]+[serv[13]]+[str(int((time.time() - starttime)*1000))]]
+	except:
+		pass
+sock.close()
+msg=''
+for key in servs.keys():
+	msg+=key+'\n'
+ 	for serv in servs[key]:
+  		msg+='    '+serv[0]+'-'+serv[1]+'-'+serv[2]+'/'+serv[3]+'-('+serv[4]+'ms)\n'
+outMsg["message"]=msg
