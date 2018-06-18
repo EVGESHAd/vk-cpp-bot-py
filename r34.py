2636852d4a90473583527c4abfa15ac2ab53911d
EVGESHAd <evgeniy.d@informmonitoring.ru>
2018-06-18 15:40:01 +0300

    рулсы подправил)

---
+++ r34.py
@@ -0,0 +1,19 @@
+﻿# -*- coding: utf-8 -*-
+import random
+import untangle
+import os
+r34text = '-fur+-scat*+-darling_in_the_franxx+-furry+-dragon+-guro+-animal_penis+-animal+-wolf+-fox+-webm+-my_little_pony+-monster*+-3d+-animal*+-ant+-insects+-mammal+-horse+-blotch+-deer+-real*+-shit+-everlasting_summer+-copro*+-wtf+-censored+' + msg.replace(' ','+')
+outMsg['message']=''
+outMsg['attachment']=''
+try:
+	resp = untangle.parse('http://0s.oj2wyzjtgqxhq6dy.cmle.ru/index.php?page=dapi&s=post&limit=10000&q=index&tags='+str(r34text)).posts.post
+	if not os.path.exists('34'):
+		os.mkdir('34')
+	indexs=range(len(resp))
+	random.shuffle(indexs)
+	for i in indexs[0:10]:
+		outMsg['message']+=str(i+1)+'/'+str(len(resp))+'\n'+str(resp[i]['tags'])+'\n\n'
+		net_download(str(resp[i]['file_url']), '34/'+str(msg_id)+'_'+str(i)+'.jpg', '')
+		outMsg['attachment']+=vk_upload('34/'+str(msg_id)+'_'+str(i)+'.jpg', outMsg['peer_id'], 'photo')+','
+except:
+	outMsg['message']+='¯\\_(ツ)_/¯'
