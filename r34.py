ee90cee53e47ea88df31d98b42c5c851fc92e6d4
EVGESHAd <evgeniy.d@informmonitoring.ru>
2018-07-04 11:31:33 +0300

    ...

--- r34.py
+++ r34.py
@@ -2,13 +2,15 @@
 import untangle
 import os
 r34text = '-fur+-scat*+-darling_in_the_franxx+-furry+-dragon+-guro+-animal_penis+-animal+-wolf+-fox+-webm+-my_little_pony+-monster*+-3d+-animal*+-ant+-insects+-mammal+-horse+-blotch+-deer+-real*+-shit+-everlasting_summer+-copro*+-wtf+-censored+' + msg.replace(' ','+')
+r34text=msg.replace(' ','+')
+r34text = '-fur+-scat*+-furry+-dragon+-guro+-animal_penis+-animal+-wolf+-fox+-webm+-my_little_pony+-monster*+-animal*+-ant+-insects+-mammal+-horse+-blotch+-deer+-real*+-shit+-everlasting_summer+-copro*+-wtf+-censored+' + msg.replace(' ','+')
 outMsg['message']=''
 outMsg['attachment']=''
 try:
 	resp = untangle.parse('http://0s.oj2wyzjtgqxhq6dy.cmle.ru/index.php?page=dapi&s=post&limit=10000&q=index&tags='+str(r34text)).posts.post
 	if not os.path.exists('34'):
 		os.mkdir('34')
-	indexs=range(len(resp))
+	indexs=list(range(len(resp)))
 	random.shuffle(indexs)
 	for i in indexs[0:10]:
 		#outMsg['message']+=str(i+1)+'/'+str(len(resp))+'\n'+str(resp[i]['tags'])+'\n\n'
