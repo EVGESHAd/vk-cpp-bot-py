aeee7862a56c0718fa4a1f49ac86a1557a548691
EVGESHAd <evgeniy.d@informmonitoring.ru>
2018-08-15 15:14:27 +0300

    удалил поиск лиц(

--- face.py
+++ face.py
@@ -1,6 +1,7 @@
 import cv2, os
 import numpy as np
 from PIL import Image
+from threading import Thread
 
 faceCascade = cv2.CascadeClassifier("py/haarcascade_frontalface_default.xml")
 
@@ -11,15 +12,20 @@ if not os.path.exists('face/imgs'):
 if not os.path.exists('face/faces'):
 	os.mkdir('face/faces')
 
-outMsg['attachment']=''
-for url in msg_imgs:
-	name = 'face/imgs/'+str(msg_id)+'_'+url.split('/')[-1]
-	net_download(url, name, '')
-	img = Image.open(name)
-	image = np.array(img.convert('L'), 'uint8')
-	faces = faceCascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),flags=cv2.CASCADE_SCALE_IMAGE)
-	i=0
-	for (x, y, w, h) in faces:
-		img.crop((x, y, x+w, y+h)).save('face/faces/'+str(i)+'-'+str(msg_id)+'.jpg', 'JPEG')
-		outMsg['attachment']+=vk_upload('face/faces/'+str(i)+'-'+str(msg_id)+'.jpg', outMsg['peer_id'], 'photo')+','
-		i+=1
+def f():
+	outMsg['attachment']=''
+	for url in msg_imgs:
+		name = 'face/imgs/'+str(msg_id)+'_'+url.split('/')[-1]
+		net_download(url, name, '')
+		img = Image.open(name)
+		image = np.array(img.convert('L'), 'uint8')
+		faces = faceCascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),flags=cv2.CASCADE_SCALE_IMAGE)
+		i=0
+		for (x, y, w, h) in faces:
+			img.crop((x, y, x+w, y+h)).save('face/faces/'+str(i)+'-'+str(msg_id)+'.jpg', 'JPEG')
+			outMsg['attachment']+=vk_upload('face/faces/'+str(i)+'-'+str(msg_id)+'.jpg', outMsg['peer_id'], 'photo')+','
+			i+=1
+
+thread = Thread(target=f, args=())
+thread.daemon = True
+thread.start()
