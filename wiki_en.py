536d91ca1bf4620a93a8d00bd689a9859cb403c4
EVGESHAd <evgeniy.d@informmonitoring.ru>
2018-09-16 13:14:07 +0300

    вики ...

--- wiki_en.py
+++ wiki_en.py
@@ -1,5 +1,10 @@
 import json
+import binascii
 outMsg['message']=''
-res=json.loads(net_send('https://en.wikipedia.org/w/api.php',{'action':'opensearch','search':msg,'limit':'3','namespace':'0','format':'json'}, 1))
-for i in range(len(res[2])):
- outMsg['message']+=res[1][i]+'\n'+res[2][i]+'\n( '+res[3][i]+' )\n\n'
+res=json.loads(net_send('https://en.wikipedia.org/w/api.php',{'action':'opensearch','search':msg,'limit':'10','namespace':'0','format':'json'}, 1))
+if len(res[2]):
+	outMsg['message']+='¤'+res[1][0]+'\n'+res[2][0]+'\n( '+binascii.a2b_qp(res[3][0].replace('%','=').encode("utf-8")).decode()+' )\n\n'
+	if len(res[2])>1:
+		outMsg['message']+='Другие варианты:\n'
+		for title in res[1][1:]:
+			outMsg['message']+='•'+title+'\n'
