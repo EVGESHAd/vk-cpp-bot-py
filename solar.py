7a3b2685e355b5823e59a2450b054d9cd9ad3b35
EVGESHAd <evgeniy.d@informmonitoring.ru>
2018-10-06 22:36:41 +0300

    fix

--- solar.py
+++ solar.py
@@ -1,2 +1,2 @@
-net_download('http://www2.irf.se/maggraphs/preliminary_k_index_last_24.png', 'solar.jpg', '')
-outMsg['attachment']=vk_upload('solar.jpg', outMsg['peer_id'], 'photo')+','
+net_download('http://www2.irf.se/maggraphs/preliminary_k_index_last_24.png', 'solar.png', '')
+outMsg['attachment']=vk_upload('solar.png', outMsg['peer_id'], 'photo')+','
