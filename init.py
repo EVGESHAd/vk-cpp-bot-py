c9e2203cd662fb894e9a633c6374d8a6060b9ed1
EVGESHAd <evgeniy.d@informmonitoring.ru>
2019-01-20 21:37:23 +0300

    ...

--- init.py
+++ init.py
@@ -1,7 +1,7 @@
 print('py init')
 #     cmd      path    disp info cost acess
 init('i', 'example.py', 1, 'info', 0, 1)
-#init('masta', 'ms_xash.py', 1, 'xash servers', 0, 1)
+init('маста', 'ms_xash.py', 1, 'xash servers', 0, 1)
 #init('music', 'music.py', 1, 'music search', 0, 1)
 init('r34', 'r34.py', 0, 'rule 34)', 0, 1)
 init('cl', 'clean.py', 1, 'clean msg in this chat', 0, 1)
--- ms_xash.py
+++ ms_xash.py
@@ -2,12 +2,16 @@ import socket
 import struct
 import time
 import re
+gamedir = msg
+if len(gamedir) == 0:
+	gamedir = 'valve'
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 adr=('ms.xash.su', 27010)
 sock.bind(('0.0.0.0', 0))
-sock.sendto(b'0.19.2\xff0.0.0.0:0\0\0', adr)
+sock.sendto(b'1\xff0.0.0.0:0\0\\gamedir\\'+gamedir.encode()+b'\\clver\\1200\\\0', adr)
 data = sock.recvfrom(4096)
-s = 6;
+#print(data)
+s = 6
 servs={}
 slots={}
 while True:
@@ -22,7 +26,7 @@ sock.setblocking(0)
 while time.time() - starttime < 2:
 	try:
 		data = sock.recvfrom(4096)
-		serv = re.sub(r'\^\d{1}', '', data[0][10:].decode()).split('\\')
+		serv = re.sub(r'\^\d{1}', '', data[0][10: ].decode()).split('\\')
 		servs[serv[15]]=servs.get(serv[15], [])+[[serv[1]]+[serv[3]]+[serv[11]]+[serv[13]]+[str(int((time.time() - starttime)*1000))]+[data[1][0]+':'+str(data[1][1])]]
 		slot=slots.get(serv[15], [0, 0])
 		slot[0]+=int(serv[11])
@@ -32,13 +36,8 @@ while time.time() - starttime < 2:
 		pass
 sock.close()
 msg=''
-slots['all']=[0, 0]
 for key in servs.keys():
 	msg+=key+'-'+str(slots[key][0])+'/'+str(slots[key][1])+'-'+str(int(slots[key][0]*100/slots[key][1]))+'%\n'
 	for serv in servs[key]:
 		msg+='    '+serv[0]+'-'+serv[1]+'-'+serv[2]+'/'+serv[3]+'-('+serv[5]+'-'+serv[4]+'ms)\n'
-		slots['all'][0]+=int(serv[2])
-		slots['all'][1]+=int(serv[3])
-key='all'
-msg+=key+'-'+str(slots[key][0])+'/'+str(slots[key][1])+'-'+str(int(slots[key][0]*100/slots[key][1]))+'%\n'
 outMsg["message"]=msg
