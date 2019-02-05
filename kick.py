24175e9752609cbc0c7162191ff8ea3afb4d568a
EVGESHAd <evgeniy.d@informmonitoring.ru>
2019-02-05 20:31:45 +0300

    fix kick

--- kick.py
+++ kick.py
@@ -1,16 +1,26 @@
 import json
+from random import randint as rand
 outMsg['message']='земля ему пухом'
 if user_get(user_id)==1:
-	outMsg["attachment"]=vk_upload("py/357_shot1.wav", outMsg["peer_id"], "audio_message");
-	vk_send('messages.send', outMsg, 1)
-	vk_send('messages.removeChatUser', {'chat_id':str(chat_id), 'user_id':str(user_id)}, 1)
+    if rand(0,100)<40:
+        outMsg["attachment"]=vk_upload("py/357_shot1.wav", outMsg["peer_id"], "audio_message")
+        vk_send('messages.send', outMsg, 1)
+        outMsg={}
+        vk_send('messages.removeChatUser', {'chat_id':str(chat_id), 'user_id':str(user_id)}, 1)
+    else:
+        outMsg["attachment"]=vk_upload("py/357_shot1.wav", outMsg["peer_id"], "audio_message")
+        vk_send('messages.send', outMsg, 1)
+        outMsg["attachment"]=vk_upload("py/357_reload1.wav", outMsg["peer_id"], "audio_message")
+        outMsg['message']='живучий попался'
+        vk_send('messages.send', outMsg, 1)
+        outMsg={}
 else:
 	try:
 		id=''
 		try:
 			id=str(json.loads(vk_send('messages.getById', {'message_ids':str(msg_id)}, 1))['response']['items'][0]['fwd_messages'][0]['user_id'])
 		except:
-			id=msg.split('/')[-1].split('|')[0].replace('[', '')
+			id=msg.split('/')[-1].split('|')[0].replace('[', '').replace('@', '')
 			id=str(json.loads(vk_send('users.get',{'user_ids':id},1))['response'][0]['id'])
 		outMsg["attachment"]=vk_upload("py/357_shot1.wav", outMsg["peer_id"], "audio_message");
 		vk_send('messages.send', outMsg, 1)
@@ -18,4 +28,4 @@ else:
 		outMsg={'peer_id':outMsg['peer_id']}
 	except:
 		outMsg['message']='¯\\_(ツ)_/¯'
-		outMsg["attachment"]=vk_upload("py/357_reload1.wav", outMsg["peer_id"], "audio_message");
+		outMsg["attachment"]=vk_upload("py/357_reload1.wav", outMsg["peer_id"], "audio_message")
